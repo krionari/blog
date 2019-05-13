dc639dc2897dfa60bb37c8485f31d85317210058
Pascal Encinas <krionari@gmail.com>
2019-05-13 12:57:21 +0200

    page default

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+// src/Controller/BlogController.php
+namespace App\Controller;
+
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_index")
+     */
+    public function index()
+    {
+        return $this->render('Blog/default.html.twig');
+    }
+}
