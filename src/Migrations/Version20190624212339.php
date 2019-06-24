347cd0604f4ee559f5c6ccee514ce63787e644a6
Pascal Encinas <krionari@gmail.com>
2019-06-24 23:26:48 +0200

    bugFix

--- src/Migrations/Version20190624212339.php
+++ src/Migrations/Version20190624212339.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20190624211401 extends AbstractMigration
+final class Version20190624212339 extends AbstractMigration
 {
     public function getDescription() : string
     {
